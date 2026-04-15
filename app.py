ee1a8bd770041763548b356643ed3efb80c13dd7
nERD8932 <samir.amin8932@gmail.com>
2026-04-15 14:39:15 -0300

    defined app in app.py to be top-level for gunicorn

--- app.py
+++ app.py
@@ -1,7 +1,2 @@
-import os
-from newsletter.db import init_db_command
 from newsletter import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app = create_app()
